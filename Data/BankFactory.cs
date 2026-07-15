8867d8e136b4636b2720d62f14cb9247da897f73
Ahmed yasir <ayasir@bwatech.sa>
2026-07-15 12:19:07 +0300

    feat: add IBankService interface

--- Data/BankFactory.cs
+++ Data/BankFactory.cs
@@ -7,48 +7,47 @@ namespace BankIntegrationSimulator.Data
 
         public static List<Bank> GetBanks()
         {
-            return new List<Bank>
-        {
-            new Bank(
-                "Saudi National Bank",
-                "SNB",
-                "Saudi Arabia",
-                "SAR",
-                "OAuth2",
-                "https://api.snb.com",
-                true,
-                true),
+            return new List<Bank> {
+                new Bank(
+                    "Saudi National Bank",
+                    "SNB",
+                    "Saudi Arabia",
+                    "SAR",
+                    "OAuth2",
+                    "https://api.snb.com",
+                    true,
+                    true),
 
-            new Bank(
-                "Al Rajhi",
-                "RJHI",
-                "Saudi Arabia",
-                "SAR",
-                "Mutual TLS",
-                "https://api.alrajhi.com",
-                true,
-                true),
+                new Bank(
+                    "Al Rajhi",
+                    "RJHI",
+                    "Saudi Arabia",
+                    "SAR",
+                    "Mutual TLS",
+                    "https://api.alrajhi.com",
+                    true,
+                    true),
 
-            new Bank(
-                "Al Riyad",
-                "RIYAD",
-                "Saudi Arabia",
-                "SAR",
-                "Mutual TLS",
-                "https://api.riyadbank.com",
-                true,
-                true),
+                new Bank(
+                    "Al Riyad",
+                    "RIYAD",
+                    "Saudi Arabia",
+                    "SAR",
+                    "Mutual TLS",
+                    "https://api.riyadbank.com",
+                    true,
+                    true),
 
-            new Bank(
-                "Mock Bank",
-                "MOCK",
-                "UK",
-                "EUR",
-                "OAuth2",
-                "https://api.mock.com",
-                true,
-                true)
-        };
+                new Bank(
+                    "Mock Bank",
+                    "MOCK",
+                    "UK",
+                    "EUR",
+                    "OAuth2",
+                    "https://api.mock.com",
+                    true,
+                    true)
+            };
         }
     }
 }
