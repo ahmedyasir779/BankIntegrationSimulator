97ed75e7272a0af0483c9198d00939573a17259b
Ahmed yasir <ayasir@bwatech.sa>
2026-07-15 13:09:31 +0300

    feat: add bank adapter abstractions

---
+++ Adapters/AlRajhiBankAdapter.cs
@@ -0,0 +1,12 @@
+﻿using BankIntegrationSimulator.Models;
+
+namespace BankIntegrationSimulator.Adapters
+{
+    internal class AlRajhiBankAdapter : IBankAdapter
+    {
+        public BalanceResponse GetBalance(Bank bank, string accountNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Adapters/MockBankAdapter.cs
@@ -0,0 +1,12 @@
+﻿using BankIntegrationSimulator.Models;
+
+namespace BankIntegrationSimulator.Adapters
+{
+    internal class MockBankAdapter : IBankAdapter
+    {
+        public BalanceResponse GetBalance(Bank bank, string accountNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
